afb0c4a96f34d996abbab18b3a89933bd7f07408
Peter Murphy <peter.francis.murphy@gmail.com>
2019-03-26 16:44:52 -0400

    Add PUT

--- api/zoos/router.js
+++ api/zoos/router.js
@@ -34,24 +34,64 @@ router.get("/:id", (_req, res) => {
 })
 
 router.post("/", (req, res) => {
-  db("zoos")
-    .insert(req.body)
-    .catch(err => {
-      res.status(404).json(err)
-    })
-    .then(([id]) => {
-      db("zoos")
-        .where({ id })
-        .catch(err => {
-          res.status(500).json(err)
+  const { name } = req.body
+  name == null
+    ? res.status(400).json({
+        message: "Please include a name."
+      })
+    : db("zoos")
+        .insert({ name })
+        .catch(_err => {
+          res.status(500).json({
+            message: "Error inserting zoo."
+          })
         })
-        .then(([zoo]) => {
-          res.status(200).json(zoo)
+        .then(([id]) => {
+          db("zoos")
+            .where({ id })
+            .catch(_err => {
+              res.status(500).json({
+                message: "Error retrieving inserted zoo."
+              })
+            })
+            .then(([zoo]) => {
+              res.status(200).json(zoo)
+            })
         })
-    })
 })
 
-router.put("/:id", (req, res) => {})
+router.put("/:id", (req, res) => {
+  const { id } = req.params
+  const { name } = req.body
+  name == null
+    ? res.status(400).json({
+        message: "Please include a name."
+      })
+    : db("zoos")
+        .where({ id })
+        .update({ name })
+        .catch(_err => {
+          res.status(500).json({
+            message: "Error updating zoo."
+          })
+        })
+        .then(num => {
+          num == 0
+            ? res.status(404).json({
+                message: "Invalid ID."
+              })
+            : db("zoos")
+                .where({ id })
+                .catch(_err => {
+                  res.status(500).json({
+                    message: "Error retrieving inserted zoo."
+                  })
+                })
+                .then(([zoo]) => {
+                  res.status(200).json(zoo)
+                })
+        })
+})
 
 router.delete("/:id", (req, res) => {})
 
